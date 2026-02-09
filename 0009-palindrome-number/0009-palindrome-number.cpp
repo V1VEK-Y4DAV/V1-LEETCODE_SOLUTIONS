33feae269ab1e06f2708a9dc46de85633b93a0b4
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-02-09 15:46:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int org = x;
+        int reversenum = 0;
+        while(x > 0){
+            int num = x % 10;
+            reversenum  = reversenum * 10 + num;
+            x /= 10;
+        }
+        return reversenum == org;
+    }
+};
