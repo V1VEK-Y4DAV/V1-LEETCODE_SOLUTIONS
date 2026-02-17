0c9a45ccd7a0c93861def3328071c642da4f6735
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-02-18 00:21:04 +0530

    Time: 168 ms (62.69%), Space: 12.7 MB (39.23%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -3,13 +3,13 @@ public:
     int countPrimes(int n) {
         if (n <= 2) return 0;
 
-        vector<bool> prime(n, true);
-        prime[0] = prime[1] = false;
+        vector<bool> prime(n, 1);
+        prime[0] = prime[1] = 0;
 
         for (int i = 2; i * i < n; i++) {
             if (prime[i]) {
                 for (int j = i * i; j < n; j += i) {
-                    prime[j] = false;
+                    prime[j] = 0;
                 }
             }
         }
