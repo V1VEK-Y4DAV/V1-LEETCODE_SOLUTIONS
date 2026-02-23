2384c6a448c704ffae1ddd071012455583df7ce3
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-02-23 11:15:45 +0530

    Time: 2 ms (78.29%), Space: 7.8 MB (36.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,7 +4,14 @@ public:
         if(n <= 1){
             return n;
         }
-
-        return fib(n - 1) + fib(n - 2);
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        for(int i = 2; i <= n; i++){
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
     }
 };
