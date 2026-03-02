8dbdbf853e8e923986e4703faaa8d792ab895cbd
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-03-02 20:26:46 +0530

    Time: 0 ms (100%), Space: 9.2 MB (76.23%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -3,14 +3,12 @@ public:
     char findTheDifference(string s, string t) {
         char ans = 0;
 
-        for(char ch : s){
-            ans ^= ch;
+        for(int i = 0; i < s.length(); i++){
+            ans ^= s[i];
         }
-
-        for(char ch: t){
-            ans ^= ch;
+        for(int i = 0; i < t.length(); i++){
+            ans ^= t[i];
         }
-
         return ans;
     }
 };
