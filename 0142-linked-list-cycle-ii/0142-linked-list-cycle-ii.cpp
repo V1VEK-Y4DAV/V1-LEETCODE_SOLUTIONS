342047ca27bbc38efcbc20a56d0faba7c50c12fe
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-03-07 21:17:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,29 +9,25 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head == NULL || head->next == NULL){
+        if(head == NULL || head -> next == NULL){
             return NULL;
         }
-
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast != NULL && fast->next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
+        while(fast != NULL && fast -> next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
 
             if(slow == fast){
                 slow = head;
-
                 while(slow != fast){
-                    slow = slow->next;
-                    fast = fast->next;
+                    slow = slow -> next;
+                    fast = fast -> next;
                 }
-
                 return slow;
             }
         }
-
         return NULL;
     }
 };
