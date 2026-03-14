ec8835addbc0d4a0fd4d8aaf0d5eb9526dc398cf
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-03-14 12:43:01 +0530

    Time: 59 ms (30.9%), Space: 18.8 MB (37.42%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,20 +4,20 @@ public:
         int row = matrix.size();
         int col = matrix[0].size();
 
-        int row_index = 0;
-        int col_index = col - 1;
+        int row_index = row - 1;
+        int col_index = 0;
 
-        while(row_index < row && col_index >= 0){
+        while(row_index >= 0 && col_index < col){
             int element = matrix[row_index][col_index];
 
             if(element == target){
                 return true;
             }
-            else if(element < target){
-                row_index++;
+            else if(element > target){
+                row_index--;
             }
             else{
-                col_index--;
+                col_index++;
             }
         }
         return false;
