8dcbf4e694949d5be4a34309d5e5addbaefeb080
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-03-16 19:12:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a + b;
+        int carry = 0;
+        while (b != 0) {
+        carry = a & b;  // Calculate the carry
+        a = a ^ b;      // Calculate the sum without carry
+        b = carry << 1; // Shift the carry to the left
+    }
+    return a;
     }
 };
