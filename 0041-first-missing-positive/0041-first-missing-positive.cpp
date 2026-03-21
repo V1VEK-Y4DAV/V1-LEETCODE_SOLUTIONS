c40d2fedd00b17ae7d425dd8419349598e7457b9
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-03-22 04:11:53 +0530

    Time: 24 ms (19.68%), Space: 55 MB (61.99%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int max = *max_element(nums.begin(), nums.end());
+        if(max < 1){
+            return 1;
+        }
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i <= max; i++){
+            if(!binary_search(nums.begin(), nums.end(), i)){
+                return i;
+            }
+        }
+        return max + 1;
+    }
+};
