c13f6bb134ced9a6b4c9daa9fba56a04f8b9988f
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-03-22 04:13:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -7,7 +7,7 @@ public:
         }
         sort(nums.begin(), nums.end());
         for(int i = 1; i <= max; i++){
-            if(!binary_search(nums.begin(), nums.end(), i)){
+            if(!binary_search(nums.begin(), nums.end(), i)){ // used binary search instead of trivial find function to avoid TLEs.
                 return i;
             }
         }
