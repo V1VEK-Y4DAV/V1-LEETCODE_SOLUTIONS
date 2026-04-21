e6c7fe336e49d9a338ae4fb902bec6ee7ad82f67
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-04-21 12:17:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
@@ -3,9 +3,9 @@ public:
     int countQuadruplets(vector<int>& nums) {
         int counter = 0;
         int n = nums.size();
-        for(int  i = 0 ;i < n - 1; i++){
-            for(int j = i + 1; j < n - 1; j++){
-                for(int k = j + 1; k < n - 1; k++){
+        for(int  i = 0 ;i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                for(int k = j + 1; k < n; k++){
                     for(int l = k + 1; l < n; l++){
                         int a = nums[i];
                         int b = nums[j];
