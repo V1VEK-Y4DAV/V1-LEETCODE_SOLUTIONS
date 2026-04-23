a61f051ad5e300a4d91f62030063aa0ada064ee6
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-04-23 13:25:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int count = 0;
+        queue<TreeNode* > q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+            q.pop();
+
+            // if(curr -> left == NULL && curr -> right == NULL){
+                count++;
+
+            if(temp -> left){
+                q.push(temp -> left);
+            }
+
+            if(temp -> right){
+                q.push(temp -> right);
+            }
+        }
+        return count;
+    }
+};
