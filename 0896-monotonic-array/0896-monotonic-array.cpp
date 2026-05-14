1fcd30a46bdc5397357560a16cbae5f95f3bea2d
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-05-14 14:11:09 +0530

    Time: 3 ms (31.73%), Space: 100.3 MB (92.46%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool increase = true;
+        bool decrease = true;
+
+        for(int i = 0; i < nums.size() - 1; i++){
+            if(nums[i] < nums[i + 1]){
+                decrease = false;
+            }
+
+            if(nums[i] > nums[i + 1]){
+                increase = false;
+            }
+        }
+
+        return increase || decrease;
+        // if(nums.size() <= 2){
+        //     return true;
+        // }
+
+        // if(nums[0] < nums[nums.size() - 1]){
+        //     int index = 0;
+        //     while(index < nums.size() - 1){
+        //         if(nums[index] > nums[index + 1]){
+        //             return false;
+        //         }
+        //         index++;
+        //     }
+        // }
+        // else if(nums[0] > nums[nums.size() - 1]){
+        //    int index = 0;
+        //     while(index < nums.size() - 1){
+        //         if(nums[index] < nums[index + 1]){
+        //             return false;
+        //         }
+        //         index++;
+        //     } 
+        // }
+        // else{
+        //     return true;
+        // }
+
+        // return true;
+    }
+};
