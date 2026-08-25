3d1d9103e024300d027003d07351718731ed045c
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-08-26 00:31:25 +0530

    Time: 0 ms (100%), Space: 18.3 MB (76.38%) - LeetHub

---
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> binaryVector;
+
+        int remainder = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            remainder = (remainder * 2 + nums[i]) % 5;
+
+            binaryVector.push_back(remainder == 0);
+        }
+
+        return binaryVector;
+    }
+};
