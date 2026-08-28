18224dd833ea73cc0c1622deee1685f6003caf0e
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-08-29 02:49:24 +0530

    Time: 3 ms (15.33%), Space: 11.4 MB (76.65%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+
+        vector<string> morsecode = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        set<string> comparing;
+        for(int i = 0; i < words.size(); i++) {
+            string temp = "";
+            for(int j = 0; j < words[i].size(); j++) {
+                int index = words[i][j] - 'a';
+                temp += morsecode[index];
+            }
+            comparing.insert(temp);
+        }
+
+        for(auto x: comparing) {
+            cout << x << " ";
+        }
+        // int counter = 0;
+        // for(int i = 0; i < comparing.size(); i++) {
+        //     for(int j = i+1; j < comparing.size(); j++) {
+        //         if(comparing[i] == comparing[j]){
+        //             counter++;
+        //         }
+        //     }
+        // }
+    
+        return comparing.size();
+    }
+};
