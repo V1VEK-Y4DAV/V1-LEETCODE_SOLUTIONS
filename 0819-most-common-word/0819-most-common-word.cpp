6561327b5b2cce71bf184ef764cf7e930fe53236
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-08-29 03:07:23 +0530

    Time: 4 ms (29.7%), Space: 12.5 MB (7.26%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+
+
+    for (char &ch : paragraph) {
+    ch = tolower(ch);
+
+    if (!isalpha(ch)) {
+        ch = ' ';
+    }
+}
+
+    vector<string> words;
+    stringstream ss(paragraph);
+
+    string word;
+
+    while (ss >> word) {
+        words.push_back(word);
+    }
+
+    // Now words contains each space-separated word
+    unordered_map<string, int> freq;
+    for(int i = 0; i < words.size(); i++) {
+        freq[words[i]]++;
+    }
+    priority_queue<pair<int, string>> pq;
+    for(auto it: freq){
+        pq.push({it.second, it.first});
+    }
+
+    while(!pq.empty()){
+        auto [freq, elem] = pq.top();
+        if(!(find(banned.begin(), banned.end(), elem) != banned.end())){
+            return elem;
+            break;
+        }
+        pq.pop();
+    }
+    return "-1";
+    }
+};
