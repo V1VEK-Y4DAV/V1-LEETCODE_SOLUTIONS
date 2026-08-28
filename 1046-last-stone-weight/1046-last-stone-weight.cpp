9d3bdc221fefcdb5fe26f906c49202aece4878ea
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-08-28 21:42:25 +0530

    Time: 6 ms (0.36%), Space: 10.1 MB (12.54%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,60 @@
+class Solution {
+private:
+    pair<int, int> first_second(vector<int>& stones) {
+    int highest = 0;
+    int secondHighest = 1;
+
+    for (int i = 1; i < stones.size(); i++) {
+
+        // New highest found
+        if (stones[i] > stones[highest]) {
+            secondHighest = highest;
+            highest = i;
+        }
+
+        // Current element can be second highest
+        else if (i != highest && stones[i] >= stones[secondHighest]) {
+            secondHighest = i;
+        }
+    }
+
+    return {highest, secondHighest};
+}
+
+
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        if(stones.size() == 2) {
+            return abs(stones[0] - stones[1]);
+        }
+
+        while (stones.size() > 1) {
+
+            pair<int, int> p = first_second(stones);
+
+            int high = p.first;
+            int s_high = p.second;
+
+            int temp = stones[high] - stones[s_high];
+            cout << stones[high] << " " << stones[s_high] << endl;
+            // Erase larger index first
+            if (high > s_high) {
+                stones.erase(stones.begin() + high);
+                stones.erase(stones.begin() + s_high);
+            }
+            else {
+                stones.erase(stones.begin() + s_high);
+                stones.erase(stones.begin() + high);
+            }
+
+            // if (temp > 0) {
+                stones.push_back(temp);
+            // }
+        }
+
+        for(int i = 0; i < stones.size(); i++) {
+            cout << stones[i] << " ";
+        }
+        return stones[0];
+    }
+};
