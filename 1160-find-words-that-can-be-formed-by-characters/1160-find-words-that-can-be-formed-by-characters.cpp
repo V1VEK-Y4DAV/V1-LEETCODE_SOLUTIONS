83b31fa80acca63cb354394d1fb8a342740d6d2b
VIVEK KUMAR YADAV <vivekkuyadav21@gmail.com>
2026-08-30 03:57:45 +0530

    Time: 24 ms (39.55%), Space: 24.3 MB (57.49%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,78 @@
+// class Solution {
+// public:
+//     int countCharacters(vector<string>& words, string chars) {
+//         vector<char> charsVector;
+//         for(int i = 0; i < chars.size(); i++) {
+//             charsVector.push_back(chars[i]);
+//         }
+
+//         vector<string> wordsVector;
+//         for(int i = 0; i < words.size(); i++) {
+//             string temp = words[i];
+//             bool flag = true;
+//             for(int j = 0; j < words[i].size(); j++) {
+//                 if(!(find(charsVector.begin(), charsVector.end(), words[i][j]) != charsVector.end())){
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+//             if(flag) wordsVector.push_back(temp);
+//         }
+
+//         int counter = 0;
+//         for(auto x: wordsVector) {
+//             counter += x.size();
+//         }
+
+//         return counter;
+        
+//     }
+// };
+
+
+
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        vector<char> charsVector;
+
+        for(int i = 0; i < chars.size(); i++) {
+            charsVector.push_back(chars[i]);
+        }
+
+        vector<string> wordsVector;
+
+        for(int i = 0; i < words.size(); i++) {
+            string temp = words[i];
+            bool flag = true;
+
+            // Make a copy because we will remove characters from it
+            vector<char> tempChars = charsVector;
+
+            for(int j = 0; j < words[i].size(); j++) {
+
+                auto it = find(tempChars.begin(), tempChars.end(), words[i][j]);
+
+                if(it == tempChars.end()) {
+                    flag = false;
+                    break;
+                }
+
+                // Remove the character after using it
+                tempChars.erase(it);
+            }
+
+            if(flag)
+                wordsVector.push_back(temp);
+        }
+
+        int counter = 0;
+
+        for(auto x : wordsVector) {
+            counter += x.size();
+        }
+
+        return counter;
+    }
+};
+
